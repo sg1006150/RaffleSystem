c86b25ee8620243d8386f82cc36d1c3b4587f716
sg1006150 <sg100615@outlook.com>
2024-06-05 10:40:04 +0800

    swagger可用

--- src/main/java/com/raffle/rafflesystem/config/SwaggerConfig.java
+++ src/main/java/com/raffle/rafflesystem/config/SwaggerConfig.java
@@ -1,31 +1,25 @@
 package com.raffle.rafflesystem.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
+
+
     @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com"))
-                .paths(PathSelectors.any()).build();
-    }
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-            .title("abcd")
-            .description("demo")
-                .version("1.0")
-                .build();
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("SpringShop API")
+                        .description("Spring shop sample application")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringShop Wiki Documentation")
+                        .url("https://springshop.wiki.github.org/docs"));
     }
 }
--- src/main/java/com/raffle/rafflesystem/controller/HelloController.java
+++ src/main/java/com/raffle/rafflesystem/controller/HelloController.java
@@ -1,12 +1,13 @@
 package com.raffle.rafflesystem.controller;
 
-import org.springframework.stereotype.Controller;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Operation(description = "abc",summary = "bedc")//为api添加注释
     @GetMapping("/hello")//处理get请求
     public String hello(String name)
     {
